77a4e091e4a53b517e6c7c9daf19f72c80ed9784
Dan Kober <koberd@grangeinsurance.com>
2019-12-19 13:30:03 -0500

    Add pokemon type lookup

--- src/api/controllers/intents/pokemonTypeIntent.js
+++ src/api/controllers/intents/pokemonTypeIntent.js
@@ -1,5 +1,27 @@
 "use strict";
 
+const pokemonList = require("../../../data/pokemon");
+
 exports.intent = function(agent) {
-  agent.add("We're in the Pokemon Type intent: " + agent.parameters["Pokemon"]);
+  const pokemon = agent.parameters["Pokemon"];
+  if (!pokemon) {
+    agent.add("You didn't provide a Pokemon name");
+    return;
+  }
+
+  const results = pokemonList.pokemon.filter(
+    p => p.Name.toUpperCase === pokemon.toUpperCase
+  );
+  if (results.length === 0) {
+    agent.add("I don't have any information about that Pokemon");
+    return;
+  }
+
+  const foundPokemon = results[0];
+  const types =
+    foundPokemon.Type.length === 1
+      ? foundPokemon.Type[0]
+      : `${foundPokemon.Type[0]} and ${foundPokemon.Type[1]}`;
+
+  agent.add(`${pokemon} is a ${types} type`);
 };
---
+++ src/data/pokemon.js
@@ -0,0 +1,21 @@
+exports.pokemon = [
+  {
+    Name: "Bulbasaur",
+    Type: ["Grass"],
+    EvolvesTo: "Ivysaur"
+  },
+  {
+    Name: "Ivysaur",
+    Type: ["Grass"],
+    EvolvesFrom: "Bulbasaur",
+    EvolvesTo: "Venusaur"
+  },
+  {
+    Name: "Venusaur",
+    Type: ["Grass"],
+    EvolvesFrom: "Ivysaur"
+  },
+  { Name: "Pikachu", Type: ["Electric"] },
+  { Name: "Snorlax", Type: ["Normal"] },
+  { Name: "Charmander", Type: ["Fire"] }
+];
