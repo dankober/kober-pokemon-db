5f83881dfede7119a12395f6521d54eab00637ca
Dan Kober <koberd@grangeinsurance.com>
2019-12-30 13:30:19 -0500

    Fix formatting on list maker

--- src/utils/listMaker.js
+++ src/utils/listMaker.js
@@ -1,13 +1,13 @@
 "use strict";
 
 exports.listMaker = function(array) {
-  if (array.length === 1) {
-    return array[0];
+  var output = `${array[0]}`;
+  for (var index = 1; index < array.length - 1; index++) {
+    output = output + `, ${array[index]}`;
   }
 
-  var output = "";
-  for (var index = 0; index < array.length - 1; index++) {
-    output = output + `, ${array[index]}`;
+  if (array.length === 1) {
+    return output;
   }
 
   return `${output} and ${array[array.length - 1]}`;
