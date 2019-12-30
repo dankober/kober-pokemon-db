ed62e1c880cbd3ca5100ff7dbf591f5c568bd370
Dan Kober <koberd@grangeinsurance.com>
2019-12-30 14:35:47 -0500

    Added resistance responses

--- src/api/controllers/intents/pokemonResistanceIntent.js
+++ src/api/controllers/intents/pokemonResistanceIntent.js
@@ -6,7 +6,9 @@ const listMaker = require("../../../utils/listMaker");
 exports.intent = function(agent) {
   var firstType = agent.parameters["Type1"];
   var secondType = agent.parameters["Type2"];
-  const resistance = agent.parameters["Resistance"];
+  const resistance =
+    agent.parameters["Resistance"] &&
+    agent.parameters["Resistance"].toUpperCase();
 
   if (!firstType) {
     agent.add("You didn't provide a valid Pokemon type");
@@ -21,23 +23,54 @@ exports.intent = function(agent) {
     return;
   }
 
-  if (!resistance) {
+  const addStrong = function() {
     if (firstType.StrongAgainst) {
       const strengths = listMaker.listMaker(firstType.StrongAgainst);
       agent.add(`${firstType.Name} is strong against ${strengths}.`);
     } else {
       agent.add(`${firstType.Name} isn't strong against anything.`);
     }
+  };
 
+  const addWeak = function() {
     if (firstType.WeakAgainst) {
       const weaknesses = listMaker.listMaker(firstType.WeakAgainst);
       agent.add(`It is weak against ${weaknesses}.`);
     } else {
       agent.add(`It isn't weak against anything`);
     }
+  };
 
-    return;
+  if (!resistance) {
+    addStrong();
+    addWeak();
+  } else {
+    if (secondType) {
+      secondType = typeList.types.find(
+        t => t.Name.toUpperCase() === secondType.toUpperCase()
+      );
+    }
+
+    if (!secondType) {
+      switch (resistance) {
+        case "STRONG":
+          addStrong();
+          break;
+        case "WEAK":
+          addWeak();
+          break;
+      }
+    } else {
+      const matched =
+        resistance === "STRONG"
+          ? firstType.StrongAgainst.find(t => t === secondType.Name)
+          : firstType.WeakAgainst.find(t => t === secondType.Name);
+
+      if (matched) {
+        agent.add("Yes, it is");
+      } else {
+        agent.add("No, it is not");
+      }
+    }
   }
-
-  agent.add("Working on it...");
 };
